d551858f314d01355475c4652155bf53ef67b431
Eliran Franco <DanitFranco@Elirans-MacBook-Pro.local>
2019-05-06 22:05:51 +0300

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <Sales />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+  import Sales from '../components/Sales.vue'
 
-export default {
-  name: "home",
-  components: {
-    HelloWorld
+  export default {
+    components: {
+      Sales
+    }
   }
-};
 </script>
